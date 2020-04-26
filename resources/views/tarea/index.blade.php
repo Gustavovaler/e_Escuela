fa8880196106e0b72ab918c13ac1061d7f361161
gustavo <gustavodevaler@gmail.com>
2020-04-26 05:26:03 -0300

    vista tareas.list

--- resources/views/tarea/index.blade.php
+++ resources/views/tarea/index.blade.php
@@ -1,5 +1,25 @@
-<h1>Idex</h1>
-@foreach ($tareas as $tarea)
-    {{$tarea }}
-    
-@endforeach
+@extends('layouts/app')
+
+@section('content')
+<div class="container">
+
+    <h1 class="text-white">Mis tareas</h1>
+
+        
+
+    @foreach ($tareas as $tarea)
+    <div class="card">
+        <div class="card-title">
+            @if ( $tarea->user == Auth::user())
+            {{ $tarea->archivo }}
+            @endif
+        </div>
+            
+    </div>
+       
+        
+    @endforeach
+
+</div>
+
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,35 +2,34 @@
 
 @section('content')
 <div class="container mt-5">
-    @if (Route::has('login'))
-    <div class="top-right links">
-    @auth
-            <a href="{{ url('/home') }}">Home</a>
-    @else
-
     <div class="row justify-center">
         <div class="col xs-6">
-            <img src="{{ asset('img/avatarM.png')}}" alt="" class="avatar-home rounded-circle" >
-            <img src="{{ asset('img/avatarF.png')}}" alt="" class="avatar-home rounded-circle">
-            
-            
-        </div>
-    
-    
+            <div class="container-avatar">
+                <img src="{{ asset('img/avatarM.png')}}" alt="" class="avatar-home rounded-circle" >
+                <img src="{{ asset('img/avatarF.png')}}" alt="" class="avatar-home rounded-circle">
+            </div>            
+        </div>    
         <div class="col xs-6">
-            <img src="{{ asset('img/profeF.png')}}" alt="" class="avatar-home rounded-circle" >
-            <img src="{{ asset('img/profeM.png')}}" alt="" class="avatar-home rounded-circle">
-            
-            
+            <div class="container-avatar">
+                <img src="{{ asset('img/profeF.png')}}" alt="" class="avatar-home rounded-circle" >
+                <img src="{{ asset('img/profeM.png')}}" alt="" class="avatar-home rounded-circle">
+            </div>         
+        </div>
+    </div>   
         
+    <div class="row mt-5 justify-center">
+        @if (Route::has('register'))
+
+        <div class="col xs-6 ">
+            <a href="{{ route('login') }}" class="btn btn-outline-dark justify-center align-center" >Acceder</a>
+            <a href="{{ route('register') }}" class="btn btn-outline-dark">Registrarme</a>
+        </div>
+        @endif
     </div>
-    @if (Route::has('register'))
-    <a href="{{ route('register') }}" class="btn btn-outline-dark">Registrarme</a>
-    @endif
-    @endauth
+        
+        
 </div>
-<a href="{{ route('login') }}" class="btn btn-outline-dark" >Acceder</a>
-@endif
-</div>
+
+
     
 @endsection
